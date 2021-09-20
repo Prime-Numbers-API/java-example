e37bf0fe899cf0f23c0ff529e80964531d223e8d
Evan Poe <evanpoe91@yahoo.com>
2021-09-20 18:42:20 -0500

    get-random-prime call working

--- com/company/Main.java
+++ com/company/Main.java
@@ -28,8 +28,17 @@ public class Main {
             //create our client
             HttpClient client = HttpClient.newHttpClient();
 
+            String apiKey = "123";
+            String base_url = "http://api.prime-numbers.io";
+            String start = "60309232769";
+            String end = "60309232769";
+            String forced_number = "60309232769";
+            String include_explanations = "true";
+            String include_prime_types_list = "true";
+            String language = "english";
+
             //build request using our URL
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonplaceholder.typicode.com/albums")).build();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(base_url + "/get-random-prime.php?key=" + apiKey + "&include_explanations=" + include_explanations + "&include_prime_types_list=" + include_prime_types_list + "start=" + start + "end=" + end + "&language=" + language)).build();
 
             //send request, apply methods, return results
             client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
