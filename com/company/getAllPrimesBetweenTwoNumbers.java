5ec1cdf5efcaac4017f18dbe267a2c55ace71494
Evan Poe <evanpoe91@yahoo.com>
2021-09-20 19:16:49 -0500

    getAllPrimesBetweenTwoNumbers working

--- com/company/getAllPrimesBetweenTwoNumbers.java
+++ com/company/getAllPrimesBetweenTwoNumbers.java
@@ -13,15 +13,15 @@ public class getAllPrimesBetweenTwoNumbers {
 
         String apiKey = "123";
         String base_url = "http://api.prime-numbers.io";
-        String start = "60309232769";
-        String end = "60309232769";
-        String forced_number = "60309232769";
+        String start = "350";
+        String end = "450";
+        String forced_number = "401";
         String include_explanations = "true";
         String include_prime_types_list = "true";
         String language = "english";
 
         //build request using our URL
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(base_url + "/get-all-primes-between-two-numbers.php?key=" + apiKey + "&include_explanations=" + include_explanations + "&include_prime_types_list=" + include_prime_types_list + "start=" + start + "end=" + end + "&language=" + language)).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(base_url + "/get-all-primes-between-two-numbers.php?key=" + apiKey + "&include_explanations=" + include_explanations + "&include_prime_types_list=" + include_prime_types_list + "&start=" + start + "&end=" + end + "&forced_number=" + forced_number + "&language=" + language)).build();
 
         //send request, apply methods, return results
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
